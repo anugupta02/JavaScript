3758517f4bcbe511c9f71a6fe7d6f921d2bff235
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-06 23:48:23 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,115 @@
 for(i=0;i<10;i++){
-console.log(i);
+//console.log(i)
 }
+
+let name = 'Anu'
+let scr = 100
+let bon = 20
+
+let totalScore = scr + bon
+//console.log(name)
+//console.log(totalScore)
+
+let firstName = 'Anu'
+ firstName = 'Akash'
+let lastName = 'Gupta'
+
+ let fullName = firstName + '$$' + lastName
+//console.log(fullName)
+//console.log(firstName + '-' + lastName)
+
+var score = 100
+score = 130
+const bonus = 50
+
+var finalScore = (score + bonus) * 1.8
+//console.log(finalScore)
+
+var complex = ((4 + 4) * 5)/2
+// console.log(complex)
+
+let tempInFahrenheit = 100
+
+//do calculation
+// (T(°F) - 32) × 5/9
+let celsius = (tempInFahrenheit - 32) * 5/9
+//console.log(celsius)
+
+//Null Values
+//temp is coming from Website API
+var temp = 0
+
+//done some calculations
+temp = 2
+//Null and undefined are completely different from 0 value
+//console.log("Current Temprature is : "+ temp)
+
+//Grade system
+
+//10 - Great job
+//5 - Work hard
+//2 - poor
+//0 - fail
+
+//Boolean - true/false
+
+let Grade = true
+//console.log(Grade)
+
+let actualGrade = 10
+actualGrade = 7
+let myGrade = (actualGrade <= 10)
+//console.log(myGrade)
+
+//If-else statements or cases :
+
+var whoIsHere = 'user'
+//whoIsHere = 'anu'
+if(whoIsHere === 'user'){
+   // console.log('Greeting message for user')
+   // console.log('Allow access to view all courses')
+}else if(whoIsHere === 'teacher'){
+    console.log('Greeting message for teacher')
+    console.log('Allow access to his courses')
+}else {
+    console.log('MESSAGE : please verify your email')
+    console.log('Send user an email for verification')
+}
+
+
+// && - AND operator - Both sides need to be true
+// || - OR operator - One side need to be true
+
+let isVerified = false
+let isLoggedIn = true
+let hasPaymentToken = true
+let isGuest = true
+
+if(!isVerified && isLoggedIn && hasPaymentToken){
+    console.log('Greeting message to user')
+    console.log('Grant access to paid course')
+}else if(isVerified || isGuest){
+    console.log('Allow free previews')
+}else{
+    console.log('MESSAGE : please contact admin')
+}
+
+//Scope :
+
+let iAmGlobal = 'someValue'
+
+if(true){
+    let iamLocal = 'someMoreValue'
+    console.log(iAmGlobal);
+    console.log(iamLocal);
+}else {
+
+}
+
+
+
+
+
+
+
+
