43b5fdbfe33e0d8c9160a70b675fefd8e662505e
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-07 22:17:12 +0530

    Create webChecker.js

---
+++ webChecker.js
@@ -0,0 +1,29 @@
+// Username and Password checking basics in JS :
+
+let userEmail = 'anu123abc'
+let user = 'anugupta'
+let password = 'astronaut5678'
+
+console.log(password.length)
+console.log(userEmail.toLowerCase())
+console.log(user.toUpperCase())
+console.log(userEmail.trim())
+
+let userChecker = function (myString) {
+    if((myString.includes(123)) && (myString.length > 6)){
+        return true
+    }
+        return false
+
+}
+
+console.log(userChecker(userEmail))
+
+let passChecker = function (pass) {
+    if((pass.includes(5678)) && (pass.length > 8)){
+        return true
+    }
+    return false
+}
+console.log(passChecker(password))
+
