a00943726b4a35baa09d50572f02e0c09fdbb32a
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-08 01:18:52 +0530

    Create coercion.js

---
+++ coercion.js
@@ -0,0 +1,14 @@
+// Coercion in JS :
+
+ console.log('5' + 5);
+ //const giveType = typeof {}
+ const giveType = typeof '5'
+ //const giveType = typeof 5
+    console.log(giveType)
+
+
+// Here true is 1 and false is 0
+const adder = true + 5
+const adder1 = false + 8
+console.log(adder)
+console.log(adder1)
