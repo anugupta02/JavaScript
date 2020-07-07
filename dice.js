ba8ce3f7ffc72fe2c67037aa6043a2920ffcf512
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-07 22:28:01 +0530

    Create dice.js

---
+++ dice.js
@@ -0,0 +1,13 @@
+// Generating a random number in JS
+
+let num1 = 22
+let num2 = 7
+
+reresult = num1/num2
+console.log(result.toFixed(2))
+
+let floatRes = result.toFixed(2)
+console.log(Math.floor(floatRes))
+console.log(Math.ceil(floatRes))
+
+console.log(Math.random() * 100)
