7fe5798f14874058cffe5a7b8c5f01104dd08a99
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-07 19:59:19 +0530

    Update myTodoV3.js

--- myTodoV3.js
+++ myTodoV3.js
@@ -1,1 +1,43 @@
+// Trello Version 3 in JS :
 
+let myTodosMeets = {
+    day: 'Monday',
+    meetings: 0,
+    meetDone: 0,
+    addMeeting: function (num) {
+        this.meetings = this.meetings + num;
+    },
+    addMeetingDone:function (num) {
+        this.meetings = this.meetings - num;
+    } ,
+    resetDay:function(){
+        this.meetings = 0;
+        this.meetDone = 0;
+        return `Your day has been reseted, U have ${this.meetings} meetings and ${this.meetDone} done meetings.`
+},
+    summary: function () {
+      return `You have ${this.meetings} meetings today!`
+    }
+}
+
+
+let myTodosMeets1 = {
+    day: 'Tuesday',
+    meetings: 12,
+    meetDone: 3,
+    addMeeting: function () {
+       // meetings = meetings;
+    }
+
+}
+
+myTodosMeets.addMeeting(4)
+
+//1. Handle meeting done
+myTodosMeets.addMeetingDone(5)
+
+console.log(myTodosMeets.summary())
+
+//2. Create a fn that can reset entire day
+console.log(myTodosMeets.resetDay())
+//myTodosMeets1.addMeeting()
