5e274b819cea23864c77c80c8bcdeb00966a1c0f
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-07 23:11:45 +0530

    Create myTodoV4.js

---
+++ myTodoV4.js
@@ -0,0 +1,44 @@
+// Trello Version 4 in JS (Search from array of objects in JS) :
+
+//const myTodosList = ['Buy Bread', 'Go to Gym', 'Record youtube videos', 'Coding', 'Sleep', 'Repeat']
+//console.log(myTodosList.indexOf('Coding'))
+
+const newTodos = [{
+    title: 'Buy Bread',
+    isDone: false
+    },{
+    title: 'Go to Gym',
+    isDone: false
+},{
+    title: 'Record youtube videos',
+    isDone: true
+}]
+
+const index = newTodos.findIndex(function (todo,index) {
+  //  console.log(todo)
+    return todo.title === 'Go to Gym'
+})
+ console.log(index)
+
+//Method 1 :
+const findTodo = function (myTodos,title) {
+    const index = myTodos.findIndex(function (todo, index) {
+        return todo.title.toLowerCase() === title.toLowerCase()
+    })
+    return myTodos[index]
+}
+
+let printMe = findTodo(newTodos, 'gO TO gym')
+console.log(printMe)
+
+
+//Method 2 :
+const findTodo = function (myTodos, title) {
+    const titleReturned = myTodos.find(function(todo,index) {
+        return todo.title.toLowerCase() === title.toLowerCase()
+    })
+    return titleReturned
+}
+
+let printMe = findTodo(newTodos, 'Go To gym')
+console.log(printMe)
