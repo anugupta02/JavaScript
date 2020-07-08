f2e1b0215931e461b66b097e0395994ff9df02d9
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-09 00:33:02 +0530

    Create advanceJS.js

---
+++ advanceJS.js
@@ -0,0 +1,57 @@
+
+// Try-Catch to avoid program freeze in JS :
+
+const convertToRs = (dollar) => {
+    if(typeof dollar === 'number'){
+        return dollar * 64
+    }else{
+        throw Error('Amount needs to be in number')
+    }
+}
+
+try{
+    const myValue = convertToRs(5)
+   // console.log(myValue)
+}
+catch (e) {
+    console.log(e.message);
+
+}
+//console.log("I'll not run if prog crashes")
+
+//Maps and For-Of loop in JS :
+
+var john = {
+    name: 'I am john',
+    age: 23,
+    isActive: true
+}
+
+var marry = {
+    name: 'I am marry',
+    age: 24,
+    isActive: true
+}
+
+var sam = {
+    name: 'I am sam',
+    age: 29,
+    isActive: false
+}
+
+let users = new Map()
+users.set('john', john)
+users.set('marry', marry)
+users.set('sam', sam)
+//console.log(typeof users)
+//console.log(users)
+console.log(users.size)
+
+// Note: Map returns iteratable: which are used for loops through it
+// Difference btw Objects & Maps is that you can find out how many objects
+// or entities are inside in the Map but u can't find out it with objects
+
+//console.log(users.get('john'))
+console.log(users.get('marry'))
+console.log(users.keys())
+console.log(users.values())
