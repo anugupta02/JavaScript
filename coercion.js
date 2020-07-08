677f27110dceb6266c97234ebbf0d59f1ff27734
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-07-08 23:54:24 +0530

    Update coercion.js

--- coercion.js
+++ coercion.js
@@ -12,3 +12,22 @@ const adder = true + 5
 const adder1 = false + 8
 console.log(adder)
 console.log(adder1)
+
+
+const loginDetails = []
+//login for getting details from DB
+
+const loginID = loginDetails[0]
+
+if(loginDetails){
+    console.log('Allow user to login')
+}else{
+    console.log('Auth failed')
+}
+
+// Values that interpret as false:
+//false
+//0
+//'' (Actual space or an Actual token)
+//null and undefined
+
